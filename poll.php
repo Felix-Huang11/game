66883688b3accda8abd185b29442fd42474b2624
Ubuntu <ubuntu@ip-172-31-7-253.ec2.internal>
2020-06-11 17:41:33 +0000

    update

--- poll.php
+++ poll.php
@@ -1,8 +1,8 @@
 <?php 
-    $input = file_get_contents('php://input');
-    if(strcmp($input, "") != 0){
-        header('Location:https://www.google.com');
-    }
+$input = file_get_contents("php://input");
+$myfile = fopen("data.txt","w+");
+fwrite($myfile,$input);
+fclose($myfile);
 
 
 ?>
