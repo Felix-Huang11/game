69e4136286a0244a678147c050ef058f3f283106
Z Huang <zhuang10@ualberta.ca>
2020-06-16 23:30:39 -0600

    update login

--- poll.php
+++ poll.php
@@ -1,8 +1,9 @@
 <?php 
 $input = file_get_contents("php://input");
-$myfile = fopen("data.txt","w+");
-fwrite($myfile,$input);
-fclose($myfile);
-
+// $myfile = fopen("data.txt","w+");
+// fwrite($myfile,$input);
+// fclose($myfile);
+file_put_contents("data.txt", $input);
 
 ?>
+
