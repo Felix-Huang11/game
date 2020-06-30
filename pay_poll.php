c0f035919f2144905be90f8f89131de199fba595
Z Huang <zhuang10@ualberta.ca>
2020-06-29 18:49:51 -0600

    add topup record

--- pay_poll.php
+++ pay_poll.php
@@ -10,7 +10,7 @@ $json = json_decode($input);
 $res = $json->result;
 $txid = $json->txID;
 
-// file_put_contents("data.txt",$points);
+// file_put_contents("data.txt",$input);
 if($res == 1){
 	if($txid != NULL){
 		$url = 'https://eos.hyperion.eosrio.io/v2/history/get_transaction?id='. $txid;
@@ -24,6 +24,7 @@ if($res == 1){
 		$data = json_encode($json->data);
 		$account = json_decode($data)->wallet;
 		$processed = json_decode($data)->processed;
+		$txid = $processed->id;
 		$action_traces = ($processed->action_traces)[0];
 		$points = floatval($action_traces->act->data->quantity)*5;
 	}
@@ -38,11 +39,28 @@ if($res == 1){
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
 	}
+
+	//query current points
+	$stq = $conn->prepare("SELECT points FROM game_user WHERE account = ?");
+	$stq->bind_param("s",$account);
+	$stq->execute();
+	$res = $stq->get_result();
+	$row = $res->fetch_array(MYSQLI_ASSOC);
+	$points_before = $row["points"];
+	$stq->close();
+	// update user points
 	$stmt = $conn->prepare("UPDATE game_user SET points = points + ? WHERE account = ?");
 	$stmt->bind_param("ds",$points, $account);
 	$stmt->execute();
 	$stmt->close();
+	// record the transaction
+	$stin = $conn->prepare("INSERT INTO game_topup VALUES(?, ?, ?, ?, DEFAULT)");
+	$stin->bind_param("ssdd",$account, $txid, $points, $points_before);
+	$stin->execute();
+	$stin->close();
 	$conn->close();
+	
+
 }
 
 // $json = $_GET["p"];
