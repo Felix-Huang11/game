f8013e1db364ff27bd44a8fdbf08b17dac45a1d0
Z Huang <zhuang10@ualberta.ca>
2020-07-03 19:49:48 -0600

    minimum withdrawal amount

--- withdraw.php
+++ withdraw.php
@@ -9,35 +9,42 @@ $password = "5DtNfLKdPdU1y6AL";
 $db_name = "goaskme_game";
 $valid = 0;
 
-$conn = new mysqli($servername, $username, $password, $db_name);
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+if($points < 500){
+	$valid = -1;
+}else{
+	$conn = new mysqli($servername, $username, $password, $db_name);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$stmt = $conn->prepare("SELECT account, wallet, points FROM game_user WHERE account = ?");
+	$stmt->bind_param("s", $_SESSION["account"]);
+	$stmt->execute();
+	$res = $stmt->get_result();
+	$row = $res->fetch_array(MYSQLI_ASSOC);
+	// var_dump($row);
+	$total_points = $row["points"];
+	$stmt->close();
+
+	if($total_points >= $points){
+
+		$stmt1 = $conn->prepare("UPDATE game_user SET points= points - ? WHERE account = ?");
+		$stmt1->bind_param("ds", $points, $_SESSION["account"]);
+		$stmt1->execute();
+		$stmt1->close();
+		$memo = "withdraw request";
+
+		$st = $conn->prepare("INSERT INTO game_withdraw(account, GAMT_amount,request_time,memo) VALUES (?, ?, DEFAULT,?)");
+		$st->bind_param("sds", $_SESSION["account"], $tokens, $memo);
+		$st->execute();
+		$st->close();
+		$valid = 1;
+	}
+
+	$conn->close();
+
 }
-$stmt = $conn->prepare("SELECT account, wallet, points FROM game_user WHERE account = ?");
-$stmt->bind_param("s", $_SESSION["account"]);
-$stmt->execute();
-$res = $stmt->get_result();
-$row = $res->fetch_array(MYSQLI_ASSOC);
-// var_dump($row);
-$total_points = $row["points"];
-$stmt->close();
 
-if($total_points >= $points){
 
-	$stmt1 = $conn->prepare("UPDATE game_user SET points= points - ? WHERE account = ?");
-	$stmt1->bind_param("ds", $points, $_SESSION["account"]);
-	$stmt1->execute();
-	$stmt1->close();
-	$memo = "withdraw request";
-
-	$st = $conn->prepare("INSERT INTO game_withdraw(account, GAMT_amount,request_time,memo) VALUES (?, ?, DEFAULT,?)");
-	$st->bind_param("sds", $_SESSION["account"], $tokens, $memo);
-	$st->execute();
-	$st->close();
-	$valid = 1;
-}
-
-$conn->close();
 
 ?>
 <!DOCTYPE html>
@@ -50,10 +57,12 @@ $conn->close();
 		var tokens = Number("<?php echo $tokens; ?>");
 		var account = "<?php echo $_SESSION["account"]; ?>";
 		var valid = Number("<?php echo $valid; ?>");
-		if(valid){
+		if(valid == 1){
 			alert("Withdraw Request: "+ account + " " + tokens + " GAMT");
-		}else{
+		}else if(valid == 0){
 			alert("Error: You don't have enough points");
+		}else{
+			alert("Error: The minimum withdrawal amount is 500 points");
 		}
 		window.location.replace("wallet.php");
 
